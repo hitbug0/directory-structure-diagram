b58ccaa26040548dbc91522e145b156835b14348
hitbug0 <hitbug.0@gmail.com>
2024-04-27 13:24:13 +0900

    first upload

---
+++ directory-structure-diagram.py
@@ -0,0 +1,37 @@
+import os
+
+NUM_INDENTS = 1
+BRANCH = " "*NUM_INDENTS + "├── "
+LEAF   = " "*NUM_INDENTS + "└── "
+LINE   = " "*NUM_INDENTS + "│   "
+
+def make_branch(item, depth, shape=BRANCH):
+    return LINE * depth + shape + item + "\n"
+
+def explore_directory(directory, depth=0):
+    result = ""
+    items = os.listdir(directory)
+    items.sort()
+    for index, item in enumerate(items):
+        path = os.path.join(directory, item)
+        if os.path.isdir(path):
+            if index == len(items) - 1:
+                result += make_branch(item, depth, shape=LEAF)
+            else:
+                result += make_branch(item, depth)
+            result += explore_directory(path, depth + 1)
+        else:
+            if index == len(items) - 1:
+                result += make_branch(item, depth, shape=LEAF)
+            else:
+                result += make_branch(item, depth)
+    return result
+
+def output_directory_structure(directory):
+    structure = explore_directory(directory)
+    with open("directory_structure.txt", "w") as file:
+        file.write(directory + "\n")
+        file.write(structure)
+
+current_directory = os.getcwd()
+output_directory_structure(current_directory)
---
+++ run.bat
@@ -0,0 +1,1 @@
+python directory-structure-diagram.py
